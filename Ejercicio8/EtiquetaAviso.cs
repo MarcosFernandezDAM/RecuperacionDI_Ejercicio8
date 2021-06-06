be328ae16cef3969d9180e86bde5e6b4f4949eee
MarcosFernandezDAM <MarcosFernandezDAM@users.noreply.github.com>
2021-06-06 13:11:07 +0200

    version 1

---
+++ Ejercicio8/EtiquetaAviso.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio8
+{
+    public enum eMarca
+    {
+        Nada,
+        Cruz,
+        Circulo,
+        Imagen
+    }
+
+
+    public partial class EtiquetaAviso : Control
+    {
+        private int y;
+        private int x;
+        private int width;
+        private int height;
+        
+        public EtiquetaAviso()
+        {
+            InitializeComponent();
+        }
+
+        private eMarca marca = eMarca.Nada;
+        [Category("Apariencia")]
+        [Description("Coloca X, Círculo, o nada")]
+        public eMarca Marca
+        {
+            set
+            {
+                marca = value;
+                this.Refresh();
+            }
+            get { return marca; }
+        }
+
+
+        private bool deg = false;
+        [Category("Apariencia")]
+        [Description("Fondo con degradado")]
+        public bool Degradado
+        {
+            set
+            {
+                deg = value;
+                this.Refresh();
+            }
+            get
+            {
+                return deg;
+            }
+        }
+
+        private Color colorIn;
+        [Category("Apariencia")]
+        [Description("Color de inicio del degradado")]
+        public Color ColorInicio
+        {
+            set
+            {
+                colorIn = value;
+                this.Refresh();
+            }
+            get
+            {
+                return colorIn;
+            }
+        }
+
+        private Color colorFin;
+        [Category("Apariencia")]
+        [Description("Color final del degradado")]
+        public Color ColorFinal
+        {
+            set
+            {
+                colorFin = value;
+                this.Refresh();
+            }
+            get
+            {
+                return colorFin;
+            }
+        }
+
+        private Image imagen;
+        [Category("Apariencia")]
+        [Description("Imagen del componente")]
+        public Image ImagenMarca
+        {
+            set
+            {
+                imagen = value;
+                this.Refresh();
+            }
+            get
+            {
+                return imagen;
+            }
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            int grosor = 0;
+            int offsetX = 0;
+            int offsetY = 0;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            if (deg)
+            {
+                LinearGradientBrush gradientColor = new LinearGradientBrush(new PointF(0, 0), new PointF(this.Width, this.Height), colorIn, colorFin);
+                g.FillRectangle(gradientColor, new RectangleF(0, 0, this.Width, this.Height));
+            }
+
+            switch (Marca)
+            {
+                case eMarca.Circulo:
+                    grosor = 5;
+                    g.DrawEllipse(new Pen(Color.Green, grosor), grosor, grosor, this.Font.Height, this.Font.Height);
+                    offsetX = this.Font.Height + grosor;
+                    offsetY = grosor;
+                    x = grosor;
+                    y = grosor;
+                    width = this.Font.Height;
+                    height = this.Font.Height;
+                    break;
+
+                case eMarca.Cruz:
+                    grosor = 5;
+                    Pen lapiz = new Pen(Color.Red, grosor);
+                    g.DrawLine(lapiz, grosor, grosor, this.Font.Height, this.Font.Height);
+                    g.DrawLine(lapiz, this.Font.Height, grosor, grosor, this.Font.Height);
+                    offsetX = this.Font.Height + grosor;
+                    offsetY = grosor / 2;
+                    x = grosor;
+                    y = grosor;
+                    width = this.Font.Height;
+                    height = this.Font.Height;
+                    lapiz.Dispose();
+                    break;
+
+                case eMarca.Imagen:
+                    if (imagen != null)
+                    {
+                        grosor = 10;
+                        int altoImagen = this.Font.Height;
+                        int anchoImagen = (imagen.Width * this.Font.Height) / imagen.Height;
+                        g.DrawImage(imagen, grosor, grosor, anchoImagen, altoImagen);
+                        offsetX = anchoImagen + grosor;
+                        offsetY = grosor;
+                        x = grosor;
+                        y = grosor;
+                        width = anchoImagen;
+                        height = altoImagen;
+                    }
+                    break;
+            }
+
+            SolidBrush b = new SolidBrush(this.ForeColor);
+            g.DrawString(this.Text, this.Font, b, offsetX + grosor, offsetY);
+            Size tam = g.MeasureString(this.Text, this.Font).ToSize();
+            this.Size = new Size(tam.Width + offsetX + grosor, tam.Height + offsetY * 2);
+            b.Dispose();
+        }
+
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            this.Refresh(); 
+        }
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+            if (e.X >= this.x && e.X <= this.x + width && e.Y >= this.y && e.Y <= height)
+            {
+                ClickEnMarca?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        [Category("Click en marca")]
+        [Description("Se lanza cuando se hace click en marca")]
+        public event EventHandler ClickEnMarca;
+    }
+}
---
+++ Form1/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.etiquetaAviso1 = new Ejercicio8.EtiquetaAviso();
+            this.etiquetaAviso2 = new Ejercicio8.EtiquetaAviso();
+            this.SuspendLayout();
+            // 
+            // etiquetaAviso1
+            // 
+            this.etiquetaAviso1.ColorFinal = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.etiquetaAviso1.ColorInicio = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.etiquetaAviso1.Degradado = true;
+            this.etiquetaAviso1.ImagenMarca = ((System.Drawing.Image)(resources.GetObject("etiquetaAviso1.ImagenMarca")));
+            this.etiquetaAviso1.Location = new System.Drawing.Point(110, 72);
+            this.etiquetaAviso1.Marca = Ejercicio8.eMarca.Imagen;
+            this.etiquetaAviso1.Name = "etiquetaAviso1";
+            this.etiquetaAviso1.Size = new System.Drawing.Size(107, 33);
+            this.etiquetaAviso1.TabIndex = 0;
+            this.etiquetaAviso1.Text = "etiquetaAviso1";
+            // 
+            // etiquetaAviso2
+            // 
+            this.etiquetaAviso2.ColorFinal = System.Drawing.Color.Black;
+            this.etiquetaAviso2.ColorInicio = System.Drawing.Color.White;
+            this.etiquetaAviso2.Degradado = true;
+            this.etiquetaAviso2.ImagenMarca = null;
+            this.etiquetaAviso2.Location = new System.Drawing.Point(110, 121);
+            this.etiquetaAviso2.Marca = Ejercicio8.eMarca.Cruz;
+            this.etiquetaAviso2.Name = "etiquetaAviso2";
+            this.etiquetaAviso2.Size = new System.Drawing.Size(101, 17);
+            this.etiquetaAviso2.TabIndex = 1;
+            this.etiquetaAviso2.Text = "etiquetaAviso2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.etiquetaAviso2);
+            this.Controls.Add(this.etiquetaAviso1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Ejercicio8.EtiquetaAviso etiquetaAviso1;
+        private Ejercicio8.EtiquetaAviso etiquetaAviso2;
+    }
+}
+
